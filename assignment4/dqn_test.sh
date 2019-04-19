0a84067d0020cc79a4d956926a94af3b97a8ee72
tianxiaohu <hutianxiao_fdu@126.com>
2019-04-18 23:26:13 -0700

    add test scripts and waiting for results

---
+++ assignment4/dqn_test.sh
@@ -0,0 +1,13 @@
+# python train_dqn.py CartPole-v0 --num_steps 100000
+# python train_dqn.py CartPole-v0 --num_steps 100000
+# python train_dqn.py CartPole-v0 --num_steps 100000
+# python train_dqn.py CartPole-v0 --num_steps 100000
+# python train_dqn.py CartPole-v0 --num_steps 100000
+
+# python train_dqn.py CartPole-v0 --num_steps 100000  --double_q
+# python train_dqn.py CartPole-v0 --num_steps 100000  --double_q
+# python train_dqn.py CartPole-v0 --num_steps 100000  --double_q
+# python train_dqn.py CartPole-v0 --num_steps 100000  --double_q
+python train_dqn.py CartPole-v0 --num_steps 100000  --double_q
+
+python train_dqn.py PongNoFrameskip-v4 --num_steps 600000
---
+++ assignment4/pg_tune.sh
@@ -0,0 +1,15 @@
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 32 -lr 0.001 --exp_name pend_b_32_lr_0.001
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 32 -lr 0.01 --exp_name pend_b_32_lr_0.01
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 32 -lr 0.1 --exp_name pend_b_32_lr_0.1
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.001 --exp_name pend_b_64_lr_0.001
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.01 --exp_name pend_b_64_lr_0.01
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.1 --exp_name pend_b_64_lr_0.1
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.001 --exp_name pend_b_128_lr_0.001
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.01 --exp_name pend_b_128_lr_0.01
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.1 --exp_name pend_b_128_lr_0.1
+
+
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.006 --exp_name pend_b_64_lr_0.006
+# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.006 --exp_name pend_b_128_lr_0.006
+
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.05 --exp_name pend_b_128_lr_0.05
