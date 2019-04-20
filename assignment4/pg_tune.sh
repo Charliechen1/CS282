ae53e3215af4bcede763fad08eab6dd909c0f6c8
tianxiaohu <hutianxiao_fdu@126.com>
2019-04-19 20:33:56 -0700

    finish hyperparameter search for policy gradient

--- assignment4/pg_tune.sh
+++ assignment4/pg_tune.sh
@@ -1,15 +1,9 @@
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 32 -lr 0.001 --exp_name pend_b_32_lr_0.001
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 32 -lr 0.01 --exp_name pend_b_32_lr_0.01
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 32 -lr 0.1 --exp_name pend_b_32_lr_0.1
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.001 --exp_name pend_b_64_lr_0.001
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.01 --exp_name pend_b_64_lr_0.01
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.1 --exp_name pend_b_64_lr_0.1
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.001 --exp_name pend_b_128_lr_0.001
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.01 --exp_name pend_b_128_lr_0.01
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.1 --exp_name pend_b_128_lr_0.1
-
-
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 64 -lr 0.006 --exp_name pend_b_64_lr_0.006
-# python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.006 --exp_name pend_b_128_lr_0.006
-
-python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 128 -lr 0.05 --exp_name pend_b_128_lr_0.05
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 512 -lr 0.001 --exp_name pend_b_512_lr_0.001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 512 -lr 0.01 --exp_name pend_b_512_lr_0.01
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 512 -lr 0.1 --exp_name pend_b_512_lr_0.1
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.001 --exp_name pend_b_1024_lr_0.001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.01 --exp_name pend_b_1024_lr_0.01
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 1024 -lr 0.1 --exp_name pend_b_1024_lr_0.1
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.001 --exp_name pend_b_2048_lr_0.001
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.01 --exp_name pend_b_2048_lr_0.01
+python train_pg.py Pendulum-v0 -ep 1000 --discount 0.99 -n 400 -e 3 -rtg -b 2048 -lr 0.1 --exp_name pend_b_2048_lr_0.1
